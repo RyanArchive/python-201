b5a9967b08a6119d6ac56a945d3b4cb5ae015cb9
Ryan Aristosa <ryanaristosa@outlook.com>
2022-04-18 11:08:33 +0800

    Add 'enumeration.py'

---
+++ enumeration.py
@@ -0,0 +1,10 @@
+names = [
+    "Stanley Gutierrez", 
+    "Winston Henry", 
+    "Vergil Presley", 
+    "Janice Row", 
+    "Valentine Burrows"
+]
+
+for index, name in enumerate(names):
+    print(f"{index + 1}. {name}")
